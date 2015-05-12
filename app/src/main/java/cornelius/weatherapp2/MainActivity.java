26d45f37d53261291b659a48487e7d29cb109b98
Tessa Olowniuk <tessaolowniuk@gmail.com>
2015-05-11 21:18:59 -0500

    New Interface -added forecast interface

--- app/src/main/java/cornelius/weatherapp2/MainActivity.java
+++ app/src/main/java/cornelius/weatherapp2/MainActivity.java
@@ -3,6 +3,7 @@ package cornelius.weatherapp2;
 import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
@@ -93,6 +94,14 @@ public class MainActivity extends ActionBarActivity
         } else {
             // Otherwise, select the previous step.
             mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
+    }
+
+    public void showMap(Uri geoLocation) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
         }
     }
 
